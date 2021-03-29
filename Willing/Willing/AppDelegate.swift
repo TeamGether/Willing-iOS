94eb421df937abfeb2a81f01c17813a5cfcf4076
skycat0212 <mr_magnet@naver.com>
2021-03-29 12:26:31 +0900

    set LoginVC as first VC

--- Willing/Willing/AppDelegate.swift
+++ Willing/Willing/AppDelegate.swift
@@ -10,11 +10,21 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if #available(iOS 13, *) {
+
+         } else {
+            window = UIWindow()
+            let rootVc = LoginViewController()
+            window?.rootViewController = rootVc
+            window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
